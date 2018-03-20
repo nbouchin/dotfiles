8446ed2470422ac301031212d84656a01aaf2696
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2018-03-20 08:53:51 +0100

    Update: re added emacs-heavy config

---
+++ emacs-heavy/emacs.d/elpa/ctable-20171005.1711/ctable-autoloads.el
@@ -0,0 +1,15 @@
+;;; ctable-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("ctable.el") (23069 1619 359521 782000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ctable-autoloads.el ends here
