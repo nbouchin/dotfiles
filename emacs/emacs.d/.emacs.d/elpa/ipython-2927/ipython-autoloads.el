b88b56cf930b0d0737d61a12d7a7e98599f77958
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2018-04-13 14:07:08 +0200

    Update: better python integration in emacs

---
+++ emacs/emacs.d/.emacs.d/elpa/ipython-2927/ipython-autoloads.el
@@ -0,0 +1,15 @@
+;;; ipython-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("ipython.el") (23248 21144 291907 900000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ipython-autoloads.el ends here
