1eb45da4736fb9df9ff1ed85628e1ad688f30510
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2018-06-01 20:12:47 +0200

    rm emacs.d

--- emacs/emacs.d/elpa/pyflakes-1.0/pyflakes-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; pyflakes-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "pyflakes" "pyflakes.el" (23301 4791 185779
-;;;;;;  210000))
-;;; Generated autoloads from pyflakes.el
-
-(autoload 'pyflakes "pyflakes" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; pyflakes-autoloads.el ends here
--- emacs/emacs.d/elpa/pyflakes-1.0/pyflakes-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pyflakes" "1.0" "run the python pyflakes checker putting hits in a grep buffer" 'nil)
