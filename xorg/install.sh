139c2196aaa79e2c1584a8b8bc1ce086024f7409
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2018-07-09 16:48:37 +0200

    Update: add inputrc and xsessionrc install script

---
+++ xorg/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+RED='\033[1;31m'
+PURPLE='\033[1;35m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo -e "${RED}Backup old xsessionrc config to ~/oldxsessionrc"${NC}
+cp ~/.xsessionrc ~/oldxsessionrc
+echo -e "${BLUE}Copy${NC} xsessionrc located at ${RED}~/.xsessionrc${NC}."
+ln -fs $PWD/xsessionrc ~/.xsessionrc
+echo -e "${GREEN}Installation${NC} done."
