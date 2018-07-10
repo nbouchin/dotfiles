92e1461e292f5a0e3a14f1a4a97deda90932f598
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2018-07-10 17:26:27 +0200

    Update: emacs bindings configuration

--- i3/install.sh
+++ i3/install.sh
@@ -8,7 +8,8 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 echo -e "${RED}Backup old config config to ~/oldvim"${NC}
-cp ~/.config ~/oldconfig
+mkdir -p ~/.config/i3/
+cp ~/.config/i3/config ~/oldconfig
 echo -e "${BLUE}Copy${NC} config located at ${RED}~/.config${NC}."
-ln -fs $PWD/config ~/.config
+ln -fs $PWD/config ~/.config/i3/config
 echo -e "${GREEN}Installation${NC} done."
