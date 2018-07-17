0a95d174daf828d79cbdc289c093ba0bb38baab8
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2018-07-17 22:09:08 +0200

    Update: Add YouCompleteMe install script

--- vim/install.sh
+++ vim/install.sh
@@ -14,15 +14,12 @@ mkdir -p ~/.vim/undodir
 mkdir -p ~/.vim/swapdir
 echo -e "${BLUE}Copy${NC} vimrc located at ${RED}~/.vimrc${NC}."
 ln -fs $PWD/vimrc ~/.vimrc
+ln -fs $PWD/ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
 ln -fs $PWD/gvimrc ~/.gvimrc
-echo -e "${GREEN}Install${NC} jedi and neovim (for if_python3 mode) with pip3."
-pip3 install jedi --user
-pip3 install neovim --user
+~/.vim/plugged/YouCompleteMe/./install.py --clang-complete
 echo -e "${PURPLE}Download${NC} vim-plug installation plugin located at ${RED}.local/share/vim/site/autoload/plug.vim${NC}."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo -e "${PURPLE}Download${NC} and Install vim plugins."
 vim +PlugInstall +q +q
-echo -e "${BLUE}Copy${NC} antares theme to awesome-vim-colorschemes vim plugin located at ${RED}~/.config/vim/plugged/awesomw-vim-colorschemes/colors/${NC}."
-ln -sf $PWD/antares.vim ~/.vim/plugged/awesome-vim-colorschemes/colors/
 echo -e "${GREEN}Installation${NC} done."
