0711951dde924613fdfd13106f4e33a159cf4725
Nicolas BOUCHINET <nbouchin@e3r2p19.42.fr>
2018-10-24 13:37:41 +0200

    Update: add ycm config

--- vim/install.sh
+++ vim/install.sh
@@ -19,17 +19,10 @@ git clone https://github.com/tpope/vim-surround.git --recursive $PLUG_START_PATH
 git clone https://github.com/SirVer/ultisnips.git --recursive $PLUG_START_PATH/
 git clone https://github.com/honza/vim-snippets.git --recursive $PLUG_START_PATH/
 git clone https://github.com/Valloric/YouCompleteMe.git --recursive $PLUG_START_PATH/
+git clone https://github.com/kien/ctrlp.vim.git --recursive $PLUG_START_PATH/
 echo -e "${BLUE}Copy${NC} vimrc located at ${RED}~/.vimrc${NC}."
 ln -fs $PWD/vimrc ~/.vimrc
-#echo -e "${PURPLE}Download${NC} vim-plug installation plugin located at ${RED}.local/share/vim/site/autoload/plug.vim${NC}."
-#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#echo -e "If ycm dont run in fedora install ncurses-compat-libs"
 ln -fs $PWD/ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
 #ln -fs $PWD/c-libft.snippets ~/.vim/plugged/vim-snippets/UltiSnips/c-libft.snippets
 #ln -fs $PWD/cpp-personal.snippets ~/.vim/plugged/vim-snippets/UltiSnips/cpp-personal.snippets
-#ln -fs $PWD/gvimrc ~/.gvimrc
-#python ~/.vim/plugged/YouCompleteMe/install.py --clang-complete
-#echo -e "${PURPLE}Download${NC} and Install vim plugins."
-#vim +PlugInstall +q +q
-#echo -e "${GREEN}Installation${NC} done."
+python $PLUG_START_PATH/YouCompleteMe/install.py --clang-complete
