e9db3ed7edcacf19dab159db937c31ff2c4dfa07
Nicolas BOUCHINET <nbouchin@e3r2p19.42.fr>
2018-10-24 14:19:15 +0200

    Update

--- vim/install.sh
+++ vim/install.sh
@@ -20,9 +20,10 @@ git clone https://github.com/SirVer/ultisnips.git --recursive $PLUG_START_PATH/
 git clone https://github.com/honza/vim-snippets.git --recursive $PLUG_START_PATH/
 git clone https://github.com/Valloric/YouCompleteMe.git --recursive $PLUG_START_PATH/
 git clone https://github.com/kien/ctrlp.vim.git --recursive $PLUG_START_PATH/
+git clone https://github.com/vim-airline/vim-airline.git $PLUG_START_PATH/
 echo -e "${BLUE}Copy${NC} vimrc located at ${RED}~/.vimrc${NC}."
 ln -fs $PWD/vimrc ~/.vimrc
 ln -fs $PWD/ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
-#ln -fs $PWD/c-libft.snippets ~/.vim/plugged/vim-snippets/UltiSnips/c-libft.snippets
-#ln -fs $PWD/cpp-personal.snippets ~/.vim/plugged/vim-snippets/UltiSnips/cpp-personal.snippets
-python $PLUG_START_PATH/YouCompleteMe/install.py --clang-complete
+ln -fs $PWD/c-libft.snippets $PLUG_START_PATH/vim-snippets/UltiSnips/c-libft.snippets
+ln -fs $PWD/cpp-personal.snippets $PLUG_START_PATH/vim-snippets/UltiSnips/cpp-personal.snippets
+python3 $PLUG_START_PATH/YouCompleteMe/install.py --clang-complete
