9101d9ec1ad2ed0772a667101354a11049eced90
nbouchin <nbouchin@localhost.localdomain>
2018-11-13 21:18:24 +0100

    UPDATE: fix vim installation script

--- vim/install.sh
+++ vim/install.sh
@@ -6,8 +6,8 @@ PURPLE='\033[1;35m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-PLUG_START_PATH="~/.vim/pack/my-plugins/start"
-PLUG_OPT_PATH="~/.vim/pack/my-plugins/opt"
+PLUG_START_PATH="$HOME/.vim/pack/my-plugins/start"
+PLUG_OPT_PATH="$HOME/.vim/pack/my-plugins/opt"
 
 echo -e "${RED}Backup old vim config to ~/oldvim"${NC}
 cp ~/.vimrc ~/oldvim
@@ -16,14 +16,14 @@ mkdir -p ~/.vim/undodir
 mkdir -p ~/.vim/swapdir
 mkdir -p ~/.vim/pack/my-plugins/{opt,start}
 echo -e "Vim plugin generation."
-git clone https://github.com/tpope/vim-surround.git --recursive $PLUG_START_PATH/
-git clone https://github.com/SirVer/ultisnips.git --recursive $PLUG_START_PATH/
-git clone https://github.com/honza/vim-snippets.git --recursive $PLUG_START_PATH/
-git clone https://github.com/Valloric/YouCompleteMe.git --recursive $PLUG_START_PATH/
-git clone https://github.com/kien/ctrlp.vim.git --recursive $PLUG_START_PATH/
-git clone https://github.com/vim-airline/vim-airline.git $PLUG_START_PATH/
-git clone https://github.com/justinmk/vim-syntax-extra.git $PLUG_START_PATH/
-git clone https://github.com/rafi/awesome-vim-colorschemes.git $PLUG_START_PATH/
+git clone https://github.com/tpope/vim-surround.git --recursive $PLUG_START_PATH/vim-surround
+git clone https://github.com/SirVer/ultisnips.git --recursive $PLUG_START_PATH/ultisnips
+git clone https://github.com/honza/vim-snippets.git --recursive $PLUG_START_PATH/vim-snippets
+git clone https://github.com/Valloric/YouCompleteMe.git --recursive $PLUG_START_PATH/YouCompleteMe
+git clone https://github.com/kien/ctrlp.vim.git --recursive $PLUG_START_PATH/ctrlp.vim
+git clone https://github.com/vim-airline/vim-airline.git $PLUG_START_PATH/vim-airline
+git clone https://github.com/justinmk/vim-syntax-extra.git $PLUG_START_PATH/vim-syntax-extra
+git clone https://github.com/rafi/awesome-vim-colorschemes.git $PLUG_START_PATH/awesome-vim-colorschemes
 echo -e "${BLUE}Copy${NC} vimrc located at ${RED}~/.vimrc${NC}."
 ln -fs $PWD/vimrc ~/.vimrc
 ln -fs $PWD/ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
