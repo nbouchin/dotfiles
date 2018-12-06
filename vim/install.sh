f6ba488f55ca2e6205e6c6f0e6d32c0b91c6b2bc
Nicolas BOUCHINET <nbouchin@e3r2p18.42.fr>
2018-12-06 08:05:06 +0100

    Update

--- vim/install.sh
+++ vim/install.sh
@@ -21,14 +21,15 @@ git clone https://github.com/SirVer/ultisnips.git --recursive $PLUG_START_PATH/u
 git clone https://github.com/honza/vim-snippets.git --recursive $PLUG_START_PATH/vim-snippets
 git clone https://github.com/Valloric/YouCompleteMe.git --recursive $PLUG_START_PATH/YouCompleteMe
 git clone https://github.com/kien/ctrlp.vim.git --recursive $PLUG_START_PATH/ctrlp.vim
-git clone https://github.com/vim-airline/vim-airline.git $PLUG_START_PATH/vim-airline
-git clone https://github.com/justinmk/vim-syntax-extra.git $PLUG_START_PATH/vim-syntax-extra
-git clone https://github.com/rafi/awesome-vim-colorschemes.git $PLUG_START_PATH/awesome-vim-colorschemes
-git clone https://github.com/vim-syntastic/syntastic.git $PLUG_START_PATH/vim-syntastic
+git clone https://github.com/vim-airline/vim-airline.git --recursive $PLUG_START_PATH/vim-airline
+git clone https://github.com/justinmk/vim-syntax-extra.git --recursive $PLUG_START_PATH/vim-syntax-extra
+git clone https://github.com/rafi/awesome-vim-colorschemes.git --recursive $PLUG_START_PATH/awesome-vim-colorschemes
+git clone https://github.com/vim-syntastic/syntastic.git --recursive $PLUG_START_PATH/vim-syntastic
 git clone https://github.com/myint/syntastic-extras.git $PLUG_START_PATH/syntastic-extras
-git clone https://github.com/tpope/vim-fugitive.git $PLUG_START_PATH/vim-fugitive
-git clone https://github.com/tpope/vim-surround.git $PLUG_START_PATH/vim-surround
-git clone https://github.com/rhysd/vim-clang-format.git $PLUG_START_PATH/vim-clang-format
+git clone https://github.com/tpope/vim-fugitive.git --recursive $PLUG_START_PATH/vim-fugitive
+git clone https://github.com/tpope/vim-surround.git --recursive $PLUG_START_PATH/vim-surround
+git clone https://github.com/rhysd/vim-clang-format.git --recursive $PLUG_START_PATH/vim-clang-format
+git clone https://github.com/vivien/vim-linux-coding-style.git --recursive $PLUG_START_PATH/vim-linux-coding-style
 echo -e "${BLUE}Copy${NC} vimrc located at ${RED}~/.vimrc${NC}."
 ln -fs $PWD/vimrc ~/.vimrc
 ln -fs $PWD/ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
