0ca85bc87a6bd286c41c7b95cf3a7df9d0d5dbb5
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2019-01-14 22:50:46 +0100

    update: changeing omz config

--- zsh/omz/install.sh
+++ zsh/omz/install.sh
@@ -2,4 +2,4 @@
 
 cp -v ~/.zshrc ~/zshrc.bak
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -v ./zshrc ~./.zshrc
+ln -sf $PWD/zshrc ~./.zshrc
