d51cae669ab6e8e3a0b9e26247f44ae5a9848ab2
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2019-01-14 22:55:39 +0100

    Update: add zsh syntax highlighting

--- zsh/omz/install.sh
+++ zsh/omz/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp -v ~/.zshrc ~/zshrc.bak
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-ln -sf $PWD/zshrc ~./.zshrc
+ln -sfv $PWD/zshrc ~/.zshrc
