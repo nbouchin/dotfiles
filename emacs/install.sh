66419023ac0b56acaa2c8bbb4951f761ee48ec1b
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2019-04-09 21:19:08 +0200

    update emacs config

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -pv ~/.emacs.d/my-lisp/
+#mkdir -pv ~/.emacs.d/my-lisp/
 ln -ifsv $PWD/emacs ~/.emacs
-ln -ifsv $PWD/my-tabbar.el ~/.emacs.d/my-lisp/my-tabbar.el
+#ln -ifsv $PWD/my-tabbar.el ~/.emacs.d/my-lisp/my-tabbar.el
