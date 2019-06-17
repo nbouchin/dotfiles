50810c91850a37b03cc7d297407d485d9ed3e2e3
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2019-06-17 12:42:59 +0200

    UPDATE(FISH) install git and z plugins

--- fish/install.sh
+++ fish/install.sh
@@ -2,3 +2,5 @@
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 ln -isv $PWD/config.fish ~/.config/fish/config.fish
+fisher add decors/fish-colored-man
+fisher add jethrokuan/z
