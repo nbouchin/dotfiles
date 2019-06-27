c76403af6ee8a6904d793c551846d3e6812cd1e2
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2019-06-27 19:37:12 +0200

    UPDATE(install.sh)

--- archlinux/install.sh
+++ archlinux/install.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 mkfs.vfat -F32 /dev/nvme1n1p1
 mkswap /dev/nvme1n1p2
@@ -9,9 +9,11 @@ mkdir /mnt/home && mount /dev/nvme0n1p1 /mnt/home
 swapon /dev/nvme1n1p2
 mkdir -p /mnt/boot/efi && mount -t vfat /dev/nvme1n1p1 /mnt/boot/efi
 
-pacstrap /mnt base
+pacstrap /mnt base base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
+cat install.sh | head -1 >> /mnt/install.sh && cat install.sh  | tail -28 >> /mnt/install.sh
 arch-chroot /mnt
+chmod +x install.sh
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
 
@@ -28,9 +30,14 @@ echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
 
 passwd
 
-pacman -S wpa_supplicant dialog wireless_tools grub efibootmgr neovim sway swaylock swayidle swaybg i3status dmenu tmux xorg xinit npm nodejs git intel-ucode mesa xf86-video-intel qemu qemu-arch-extra openssh
+pacman -S wpa_supplicant dialog wireless_tools grub efibootmgr neovim sway swaylock swayidle swaybg i3status dmenu tmux xorg xorg-xinit npm nodejs git intel-ucode mesa xf86-video-intel qemu qemu-arch-extra openssh python bc
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconig -o /boot/grub/grub.cfg
+grub-mkconfig -o /boot/grub/grub.cfg
+
+useradd -m nbouchin
+usermod -a -G wheel,video nbouchin
+passwd nbouchin
+exit
 exit
 umount -R /mnt
 reboot
